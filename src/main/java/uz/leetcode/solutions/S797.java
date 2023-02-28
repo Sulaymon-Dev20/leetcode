b3574ccb785b491a92696935c117274c430d2f8a
MacBookPro <sulaymon1w@gmail.com>
2023-02-28 11:54:12 +0500

    797. All Paths From Source to Target best solution but not mine

--- src/main/java/uz/leetcode/solutions/S797.java
+++ src/main/java/uz/leetcode/solutions/S797.java
@@ -1,8 +1,6 @@
 package uz.leetcode.solutions;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class S797 {
     public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
@@ -20,5 +18,38 @@ public class S797 {
         if (graph.length - 1 == id) {
             list.add(Arrays.stream(text.substring(1).split(",")).map(Integer::parseInt).toList());
         }
+    }
+
+    public List<List<Integer>> allPathsSourceTarget2(int[][] graph) {
+        return new ArrayList<>() {
+            List<List<Integer>> listOfPaths = null;
+            final Deque<Integer> pathQueue = new ArrayDeque<>();
+
+            @Override
+            public List<Integer> get(int i) {
+                return listOfPaths.get(i);
+            }
+
+            @Override
+            public int size() {
+                if (listOfPaths == null) {
+                    listOfPaths = new ArrayList<>();
+                    helper(0);
+                }
+                return listOfPaths.size();
+            }
+
+            private void helper(int current) {
+                pathQueue.addLast(current);
+                if (current == graph.length - 1) {
+                    listOfPaths.add(new ArrayList<>(pathQueue));
+                } else {
+                    for (int v : graph[current]) {
+                        helper(v);
+                    }
+                }
+                pathQueue.pollLast();
+            }
+        };
     }
 }
