2721dcaf1e72a8a85d70b0833dddb2c483cd78df
MacBookPro <sulaymon1w@gmail.com>
2023-03-10 17:32:17 +0500

    1110. Delete Nodes And Return Forest best case

--- src/main/java/uz/leetcode/solutions/S1110.java
+++ src/main/java/uz/leetcode/solutions/S1110.java
@@ -2,11 +2,29 @@ package uz.leetcode.solutions;
 
 import uz.leetcode.model.TreeNode;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class S1110 {
+    Set<Integer> to_delete_set;
+    List<TreeNode> res;
+
+    public List<TreeNode> delNodes2(TreeNode root, int[] to_delete) {
+        to_delete_set = new HashSet<>();
+        res = new ArrayList<>();
+        for (int i : to_delete) to_delete_set.add(i);
+        helper(root, true);
+        return res;
+    }
+
+    private TreeNode helper(TreeNode node, boolean is_root) {
+        if (node == null) return null;
+        boolean deleted = to_delete_set.contains(node.val);
+        if (is_root && !deleted) res.add(node);
+        node.left = helper(node.left, deleted);
+        node.right = helper(node.right, deleted);
+        return deleted ? null : node;
+    }
+
     public static List<TreeNode> delNodes(TreeNode root, int[] to_delete) {
         final ArrayList<TreeNode> list = new ArrayList<>();
         final List<Integer> list2 = new ArrayList<>(Arrays.stream(to_delete).boxed().distinct().toList());
