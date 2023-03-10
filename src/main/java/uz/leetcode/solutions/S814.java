f741b79d6e6fef75235c401b2c999297e5486c85
MacBookPro <sulaymon1w@gmail.com>
2023-03-10 15:18:38 +0500

    814. Binary Tree Pruning best case

--- src/main/java/uz/leetcode/solutions/S814.java
+++ src/main/java/uz/leetcode/solutions/S814.java
@@ -3,6 +3,13 @@ package uz.leetcode.solutions;
 import uz.leetcode.model.TreeNode;
 
 public class S814 {
+    public TreeNode pruneTree2(TreeNode root) {
+        if (root == null) return null;
+        root.left = pruneTree(root.left);
+        root.right = pruneTree(root.right);
+        return root.left == null && root.right == null && root.val == 0 ? null : root;
+    }
+
     public TreeNode pruneTree(TreeNode root) {
         if (root != null) {
             if (!checkTree(root)) {
--- src/main/java/uz/leetcode/task/Leetcode.java
+++ src/main/java/uz/leetcode/task/Leetcode.java
@@ -47,16 +47,10 @@ public class Leetcode {
     }
 
     public static TreeNode pruneTree(TreeNode root) {
-        if (root != null) {
-            if (!checkTree(root)) {
-                return null;
-            } else {
-                root.left = pruneTree(root.left);
-                root.right = pruneTree(root.right);
-            }
-            return root;
-        }
-        return root;
+        if (root == null) return null;
+        root.left = pruneTree(root.left);
+        root.right = pruneTree(root.right);
+        return root.left == null && root.right == null && root.val == 0 ? null : root;
     }
 
     public static boolean checkTree(TreeNode root) {
